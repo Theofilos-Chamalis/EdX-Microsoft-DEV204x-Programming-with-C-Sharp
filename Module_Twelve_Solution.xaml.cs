927a15910b9ec2a6a3bdc826e889a2f18969d94f
Theofilos Chamalis <theofxam@gmail.com>
2016-07-02 17:53:47 +0300

    Uploaded Module Twelve Solution

---
+++ Module_Twelve_Solution.xaml.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+
+namespace GUI_Application12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnWriteFile_Click(object sender, RoutedEventArgs e)
+        {
+           await WriteFile();
+        }
+
+        private async void btnReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            await ReadFile();
+        }
+
+        public async Task WriteFile()
+        {
+            string filePath = @"SampleFile.txt";
+            string text = txtContents.Text;
+
+            await WriteTextAsync(filePath, text);
+        }
+
+        private async Task WriteTextAsync(string filePath, string text)
+        {
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Append, FileAccess.Write, FileShare.None,
+                bufferSize: 4096,useAsync:true))
+            {
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+            };
+        }
+
+        public async Task ReadFile()
+        {
+            string filePath = @"SampleFile.txt";
+
+            if (File.Exists(filePath) == false)
+            {
+                MessageBox.Show(filePath + " not found", "File Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                try
+                {
+                    string text = await ReadTextAsync(filePath);
+                    txtContents.Text = text;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private async Task<string> ReadTextAsync(string filePath)
+        {
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096,useAsync:true))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
